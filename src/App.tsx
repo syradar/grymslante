baae6957e9f346906be3ed605d2a26871b5c8c64
David Malmström <syntex@outlook.com>
2021-01-14 08:22:11 +0100

    comment unused dice

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import "./App.css";
 const rollDice = (min: number, max: number) => () =>
   min + Math.floor(Math.random() * (max - min + 1));
 
-const d2 = rollDice(1, 2);
-const d3 = rollDice(1, 3);
-const d5 = rollDice(1, 5);
+//const d2 = rollDice(1, 2);
+//const d3 = rollDice(1, 3);
+//const d5 = rollDice(1, 5);
 const d10 = rollDice(1, 10);
-const d20 = rollDice(1, 20);
+//const d20 = rollDice(1, 20);
 
 type ExplodingRange = 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
