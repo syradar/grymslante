aa583a4e8568d2601b5f692425bc6bffdb80c1d5
David Malmström <syntex@outlook.com>
2021-01-16 22:39:15 +0100

    add rudimentary navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,20 +4,21 @@ import tw from 'twin.macro';
 import React from 'react';
 
 import { Link } from 'react-router-dom';
+import { card } from '../styles';
 
 const Navbar = () => (
-  <nav tw="mb-5 dark:bg-blue-400 light:bg-red-400">
-    <ul>
-      <li>
+  <nav tw="mb-5" css={card}>
+    <ul tw="grid grid-flow-col gap-x-3">
+      <li tw="hover:text-blue-400">
         <Link to="/">Home</Link>
       </li>
-      <li>
+      <li tw="hover:text-blue-400">
         <Link to="/skills">Skills</Link>
       </li>
-      <li>
+      <li tw="hover:text-blue-400">
         <Link to="/dice">Dice</Link>
       </li>
-      <li>
+      <li tw="hover:text-blue-400">
         <Link to="/names">Names</Link>
       </li>
     </ul>
