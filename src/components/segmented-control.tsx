f4154e9264c2f50c6898fa9e4bcd14cb6db2261d
David Malmström <syntex@outlook.com>
2021-01-17 07:45:30 +0100

    add exploding dice

--- src/components/segmented-control.tsx
+++ src/components/segmented-control.tsx
@@ -22,37 +22,33 @@ export function SegmentedControl({
         }}
       >
         {segments.map((s, index) => (
-          <>
-            <button
-              key={index}
-              tw="px-3 py-2 z-10 rounded-xl relative after:bg-gray-400 dark:after:bg-gray-500"
-              css={{
-                outline: 'none !important',
-                ':focus': {
-                  boxShadow: '0 0 4px 0 rgba(96, 165, 250, 1.1)',
-                },
-                ':after': {
-                  content: '""',
-                  display: 'block',
-                  position: 'absolute',
-                  right: '-0.25rem',
-                  width: '0.01rem',
-                  height: '1.5rem',
-                  top: '50%',
-                  opacity: `${
-                    index === selectedIndex || index === selectedIndex - 1
-                      ? 0
-                      : 1
-                  }`,
-                  transition: 'opacity 150ms ease-in-out',
-                  transform: 'translateY(-50%)',
-                },
-              }}
-              onClick={() => onSegmentClick(index)}
-            >
-              {s}
-            </button>
-          </>
+          <button
+            key={index}
+            tw="px-2 py-2 z-10 rounded-xl relative after:bg-gray-400 dark:after:bg-gray-500"
+            css={{
+              outline: 'none !important',
+              ':focus': {
+                boxShadow: '0 0 4px 0 rgba(96, 165, 250, 1.1)',
+              },
+              ':after': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                right: '-0.25rem',
+                width: '0.01rem',
+                height: '1.5rem',
+                top: '50%',
+                opacity: `${
+                  index === selectedIndex || index === selectedIndex - 1 ? 0 : 1
+                }`,
+                transition: 'opacity 150ms ease-in-out',
+                transform: 'translateY(-50%)',
+              },
+            }}
+            onClick={() => onSegmentClick(index)}
+          >
+            {s}
+          </button>
         ))}
         <div
           tw="h-full z-0 absolute transition-transform shadow-md rounded-xl bg-white dark:bg-gray-500"
