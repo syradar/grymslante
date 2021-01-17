2693e6d6e57089b683c44826914d4da49846c8ca
David Malmström <syntex@outlook.com>
2021-01-17 18:14:00 +0100

    Add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,17 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import tw from 'twin.macro';
+import React from 'react';
+
+const Footer = () => (
+  <footer tw="py-4 px-3 bg-gray-100 dark:bg-gray-800 grid grid-flow-col flex-shrink-0 text-sm dark:text-gray-400">
+    <a tw="text-red-500 hover:text-red-400 hover:underline" href="grymslante">
+      GitHub
+    </a>
+    <div tw="text-right">
+      Created with ❤️ by David Malmström &copy; {new Date().getFullYear()}
+    </div>
+  </footer>
+);
+
+export default Footer;
