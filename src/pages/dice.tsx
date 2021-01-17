99eb24efd17ab8afedce2d53f5ff24ace84a7177
David Malmström <syntex@outlook.com>
2021-01-17 05:06:37 +0100

    add segmented control

--- src/pages/dice.tsx
+++ src/pages/dice.tsx
@@ -3,12 +3,22 @@
 import { useState } from 'react';
 import tw, { css } from 'twin.macro';
 
+import { SegmentedControl } from '../components/segmented-control';
 import { explodingD10 } from '../utils/dice-roller';
 
 export const Dice = () => {
+  const [active, setActive] = useState(0);
+
+  const handleSegmentClick = (index: number) => setActive(index);
 
   return (
     <>
+      <SegmentedControl
+        segments={['d5', 'd10', 'd20']}
+        // segments={['d5', 'd10']}
+        selectedIndex={active}
+        onSegmentClick={handleSegmentClick}
+      ></SegmentedControl>
       {explodingD10(7, 3)}
     </>
   );
