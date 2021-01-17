47e0ae4eb2699cd3b9bfdc308ce12cca4621e9eb
David Malmström <syntex@outlook.com>
2021-01-17 21:02:23 +0100

    add correct github link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,10 @@ import React from 'react';
 
 const Footer = () => (
   <footer tw="py-4 px-3 bg-gray-100 dark:bg-gray-800 grid grid-flow-col flex-shrink-0 text-sm dark:text-gray-400">
-    <a tw="text-red-500 hover:text-red-400 hover:underline" href="grymslante">
+    <a
+      tw="text-red-500 hover:text-red-400 hover:underline"
+      href="https://github.com/syradar/grymslante"
+    >
       GitHub
     </a>
     <div tw="text-right">
