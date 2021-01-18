ab4616bff35a5f438a9b46d7f8ed2a6e391ab371
David Malmström <syntex@outlook.com>
2021-01-18 21:27:46 +0100

    create generic name gen and use for village, inn and plants

---
+++ src/components/generic-name-generator.tsx
@@ -0,0 +1,56 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import tw, { css } from 'twin.macro';
+import { useState } from 'react';
+
+import { buttonPrimary, card } from '../styles';
+
+import { NameList } from './name-list';
+import { capitalize, choice, range } from '../utils/utils';
+
+type NameGeneratorResource = {
+  prefix: string[];
+  suffix: string[];
+};
+
+interface GenericNameGeneratorProps {
+  json: NameGeneratorResource;
+  label: string;
+  maxNames?: number;
+}
+
+export const GenericNameGenerator = ({
+  json,
+  label,
+  maxNames = 10,
+}: GenericNameGeneratorProps) => {
+  const [nameResult, setNameResult] = useState<string[]>([]);
+
+  const names: NameGeneratorResource = json;
+
+  const handleGenerateNameClick = () =>
+    setNameResult(
+      range(maxNames)
+        .map((_) => `${choice(names.prefix)}${choice(names.suffix)}`)
+        .map(capitalize)
+    );
+
+  return (
+    <div tw="flex flex-col justify-center">
+      <div tw="flex justify-center">
+        <button
+          tw="mb-5"
+          css={buttonPrimary}
+          onClick={() => handleGenerateNameClick()}
+        >
+          Generate {label} names
+        </button>
+      </div>
+      <div tw="text-center">
+        {nameResult.length !== 0 && (
+          <NameList heading={label} names={nameResult}></NameList>
+        )}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/names-people.tsx
@@ -0,0 +1,130 @@
+/** @jsxImportSource @emotion/react */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import tw, { css } from 'twin.macro';
+import { useState } from 'react';
+
+import { SegmentedControl } from '../components/segmented-control';
+import { buttonPrimary, card } from '../styles';
+
+import peopleNames from '../data/people-names.json';
+import { NameList } from './name-list';
+import { capitalize, choice, range } from '../utils/utils';
+
+export const NamesPeople = () => {
+  const segments = [
+    'Mittlander',
+    'Stormlander',
+    'Virann',
+    'Elf',
+    'Dwarf',
+    'Troll',
+  ];
+
+  const emptyNames = {
+    male: [],
+    female: [],
+    both: [],
+  };
+
+  const [nameResult, setNameResult] = useState({ ...emptyNames });
+
+  const [active, setActive] = useState(0);
+
+  const handleSegmentClick = (index: number) => {
+    setActive(index);
+    setNameResult({ ...emptyNames });
+  };
+
+  type Suffix = {
+    [type in string]?: string[];
+  };
+
+  type Names = {
+    people: string;
+    prefix: string[];
+    suffix: Suffix;
+  };
+
+  const handleGenerateNameClick = () => {
+    const type = segments[active].toLowerCase();
+
+    const names: Names | undefined = peopleNames.find(
+      (pn) => pn.people === type
+    );
+
+    if (names) {
+      const generatedNames = Object.keys(names.suffix)
+        .filter((k) => names.suffix[k]?.length !== 0)
+        .map((k) => {
+          const suffixes = names.suffix[k];
+
+          return {
+            [k]:
+              suffixes !== undefined
+                ? range(10)
+                    .map((_) => `${choice(names.prefix)}${choice(suffixes)}`)
+                    .map(capitalize)
+                : [''],
+          };
+        })
+        .reduce((acc, cur) => ({ ...acc, ...cur }), {});
+
+      setNameResult(() => ({
+        ...emptyNames,
+        ...generatedNames,
+      }));
+    }
+  };
+
+  return (
+    <div tw="flex flex-col justify-center">
+      <div tw="mb-12">
+        <div tw="md:hidden">
+          <SegmentedControl
+            vertical={true}
+            segments={segments}
+            selectedIndex={active}
+            onSegmentClick={(index) => handleSegmentClick(index)}
+          ></SegmentedControl>
+        </div>
+        <div tw="hidden md:block">
+          <SegmentedControl
+            vertical={false}
+            segments={segments}
+            selectedIndex={active}
+            onSegmentClick={(index) => handleSegmentClick(index)}
+          ></SegmentedControl>
+        </div>
+      </div>
+      <div tw="flex justify-center mb-12">
+        <button tw="mb-5" css={buttonPrimary} onClick={handleGenerateNameClick}>
+          Generate names
+        </button>
+      </div>
+      <div
+        tw="grid gap-x-2"
+        css={{
+          gridTemplateColumns: `repeat(${
+            Object.values(nameResult).filter((nr) => nr.length !== 0).length
+          }, minmax(0, 1fr))`,
+        }}
+      >
+        <div tw="text-center">
+          {nameResult.male.length !== 0 && (
+            <NameList heading="Male" names={nameResult.male}></NameList>
+          )}
+        </div>
+        <div tw="text-center">
+          {nameResult.female.length !== 0 && (
+            <NameList heading="Female" names={nameResult.female}></NameList>
+          )}
+        </div>
+        <div tw="text-center">
+          {nameResult.both.length !== 0 && (
+            <NameList heading="Both" names={nameResult.both}></NameList>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
