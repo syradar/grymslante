4b23b8f752f3658f1ba27fddd26286fb7ddc2afb
David Malmström <syntex@outlook.com>
2021-01-18 07:57:24 +0100

    add roll history, rolled dice, improve what to roll display

--- src/styles.ts
+++ src/styles.ts
@@ -1,16 +1,31 @@
-import tw from "twin.macro";
+import tw from 'twin.macro';
 
 export const card = () => [
-    tw`rounded-lg p-3 shadow light:bg-white dark:bg-gray-700`
-]
+  tw`rounded-lg p-3 shadow light:bg-white dark:bg-gray-700`,
+];
 
 export const buttonPrimary = () => [
-    tw`rounded-lg py-2 px-3 
+  tw`rounded-lg py-2 px-3 
     bg-red-600 text-white 
     hover:bg-red-500 
     transition-all
     shadow hover:shadow-lg
     transform-gpu
     translate-y-0
-    hover:-translate-y-px`
-]
+    hover:-translate-y-px`,
+];
+
+export const buttonSubtle = () => [
+  tw`rounded-lg py-0.5 px-1 
+    bg-gray-600 text-white 
+    hover:bg-gray-500 
+    transition-all
+    shadow hover:shadow-lg
+    transform-gpu
+    translate-y-0
+    hover:-translate-y-px`,
+];
+
+export const h2Style = () => [tw`mb-3 text-xl font-bold`];
+
+export const h3Style = () => [tw`mb-2 text-lg font-bold`];
--- src/utils/dice-roller.ts
+++ src/utils/dice-roller.ts
@@ -1,23 +1,41 @@
 export const rollDice = (min: number, max: number) => () =>
   min + Math.floor(Math.random() * (max - min + 1));
 
-export const d2 = rollDice(1, 2);
-export const d3 = rollDice(1, 3);
-export const d5 = rollDice(1, 5);
-export const d10 = rollDice(1, 10);
-export const d20 = rollDice(1, 20);
-
 export type ExplodingRange = 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
-export const validExplodeRange = (value: number): value is ExplodingRange => value <= 10 && value >= 2;
+export const validExplodeRange = (value: number): value is ExplodingRange =>
+  value <= 10 && value >= 2;
 
-const exploding = (ed: ExplodingRange = 10, sum: number = 0): number => {
-  const result = d10();
-  const newSum = sum + result;
-
-  if (result < ed) return newSum;
-
-  return exploding(ed, newSum);
+export type DiceResults = {
+  sum: number;
+  rolls: number[];
 };
 
-export const explodingD10 = (ed: ExplodingRange = 10) =>
-  exploding(ed);
+export const rollDiceWithRolls = (max: number) => (): DiceResults => {
+  const roll = rollDice(1, max)();
+  return {
+    sum: roll,
+    rolls: [roll],
+  };
+};
+
+export const d2 = rollDiceWithRolls(2);
+export const d3 = rollDiceWithRolls(3);
+export const d5 = rollDiceWithRolls(5);
+export const d10 = rollDiceWithRolls(10);
+export const d20 = rollDiceWithRolls(20);
+
+const exploding = (
+  ed: ExplodingRange = 10,
+  rolls: number[] = [],
+  sum: number = 0
+): DiceResults => {
+  const result = d10();
+  const newRolls = [...rolls, result.sum];
+  const newSum = sum + result.sum;
+
+  if (result.sum < ed) return { sum: newSum, rolls: newRolls };
+
+  return exploding(ed, newRolls, newSum);
+};
+
+export const explodingD10 = (ed: ExplodingRange = 10) => exploding(ed);
