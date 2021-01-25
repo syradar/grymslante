b6a905d571461cd0baf62886f3a3b3053d25571c
David Malmström <syntex@outlook.com>
2021-01-25 22:37:17 +0100

    translate navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,50 +1,65 @@
 /** @jsxImportSource @emotion/react */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import tw from 'twin.macro';
+import tw, { css } from 'twin.macro';
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
-const Navbar = () => (
-  <nav tw="mb-12 bg-gray-300 dark:bg-gray-700">
-    <ul tw="grid grid-flow-col gap-x-3">
-      <li tw="">
-        <Link
-          css={tw`flex justify-center items-center text-center py-3 px-2 hover:bg-red-500 hover:text-white hover:transition-colors`}
-          to="/names"
-        >
-          Names
-        </Link>
-      </li>
-      <li tw="">
-        <Link
-          css={tw`flex justify-center items-center text-center py-3 px-2 hover:bg-red-500 hover:text-white hover:transition-colors`}
-          to="/items"
-        >
-          Items
-        </Link>
-      </li>
-      <li tw="">
-        <Link
-          css={tw`flex justify-center items-center text-center py-3 px-2 hover:bg-red-500 hover:text-white hover:transition-colors`}
-          to="/skills"
-        >
-          <span tw="">Skills</span>
-          <span tw="flex items-center ml-1 p-0.5 bg-gray-300 dark:bg-gray-600 text-xs leading-none font-bold rounded text-gray-400">
-            DODT
-          </span>
-        </Link>
-      </li>
-      <li tw="">
-        <Link
-          css={tw`flex justify-center items-center text-center py-3 px-2 hover:bg-red-500 hover:text-white hover:transition-colors`}
-          to="/dice"
-        >
-          Dice
-        </Link>
-      </li>
-    </ul>
-  </nav>
-);
+interface NavbarItem {
+  label: string;
+  path: string;
+  chip?: string;
+}
+
+const Navbar = () => {
+  const { t } = useTranslation('core');
+
+  const pages: NavbarItem[] = [
+    {
+      label: 'Names',
+      path: '/names',
+    },
+    {
+      label: 'Items',
+      path: '/items',
+    },
+    {
+      label: 'Skills',
+      path: '/skills',
+      chip: 'DODT',
+    },
+    {
+      label: 'Dice',
+      path: '/dice',
+    },
+  ];
+
+  return (
+    <nav tw="mb-12 bg-gray-300 dark:bg-gray-700 flex justify-center">
+      <ul tw="grid grid-flow-col gap-x-3 container">
+        {pages.map((p) => (
+          <li key={p.path}>
+            <NavLink
+              css={[
+                tw`flex flex-col h-full justify-center items-center text-center 
+                py-3 px-2 hover:bg-gray-400 dark:bg-gray-700 border-b-2 border-gray-300 dark:border-gray-700`,
+              ]}
+              to={p.path}
+              activeStyle={tw`font-bold border-red-500`}
+            >
+              <span>{t(p.label)}</span>
+              {p.chip && (
+                <span tw="flex items-center ml-1 py-0.5 px-1 bg-gray-300 dark:bg-gray-600 text-xs font-bold rounded text-gray-400">
+                  {t(p.chip)}
+                </span>
+              )}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
